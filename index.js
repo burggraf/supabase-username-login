fcecdf0c7b2b34a3d754e9d7f20b09409c5658b9
Mark Burggraf <markb@mantisbible.com>
2022-11-03 11:24:36 -0700

    setup login screen

--- index.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://qmregqvlvbsiurgktjdg.supabase.co', 
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtcmVncXZsdmJzaXVyZ2t0amRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTUxMTgsImV4cCI6MTk4MzA3MTExOH0.gWgeSkw0-zURVV6OA7Ea0-fftUQR688NEuws0mgWTN4');
-
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
 <script>
+	import { createClient } from '@supabase/supabase-js'
+	const supabase = createClient('https://qmregqvlvbsiurgktjdg.supabase.co', 
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtcmVncXZsdmJzaXVyZ2t0amRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTUxMTgsImV4cCI6MTk4MzA3MTExOH0.gWgeSkw0-zURVV6OA7Ea0-fftUQR688NEuws0mgWTN4');
+	const signUp = async () => {
+
+	}
+	const signIn = async () => {
+
+	}
 </script>
 
 <svelte:head>
@@ -7,7 +16,27 @@
 </svelte:head>
 
 <main>
-	<h1>hello</h1>
+	<h1>Demo: Supabase Login with Username</h1>
+	Username<br /><input id="username" type="text" placeholder="username" /><br />
+	<br />
+	Password<br /><input id="password" type="password" placeholder="password" /><br />
+	<br /><br />
+	<button on:click={signUp}>Sign Up</button><br /><br />
+	<button on:click={signIn}>Login</button>
 </main>
 <style>
+	main {
+		padding: 1em;
+	}
+	input {
+		/* rounded corners */
+		border-radius: 0.5em;
+		height: 2em;
+	}
+	button {
+		/* rounded corners */
+		border-radius: 0.5em;
+		height: 2em;
+		width: 150px;
+	}
 </style>
