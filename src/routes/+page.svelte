31372f072801c981040b01960a294270885b3eea
Mark Burggraf <markb@mantisbible.com>
2022-11-03 15:10:01 -0700

    username validation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,31 +2,55 @@
 	import { createClient, SupabaseClient } from '@supabase/supabase-js'
 	const supabase: SupabaseClient = createClient('https://qmregqvlvbsiurgktjdg.supabase.co', 
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtcmVncXZsdmJzaXVyZ2t0amRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTUxMTgsImV4cCI6MTk4MzA3MTExOH0.gWgeSkw0-zURVV6OA7Ea0-fftUQR688NEuws0mgWTN4');
+
+	const userNameCheck = (username: string) => {
+		if (username.length < 3 || username.length > 20)
+		return "Username must be between 3 and 20 characters";
+		if (username.indexOf(' ') > -1)
+		return "Username cannot contain spaces";
+		if (username.indexOf('@') > -1)
+		return "Username cannot contain @";
+		// only allow valid email characters
+		if (!/^[a-z0-9_]+$/.test(username))
+		return "Username can only contain letters, numbers, and underscores";
+		return null;
+	}
 	const signUp = async () => {
 		console.log('signing up');
 		const username_input: any = document.getElementById('username');
 		const password_input: any = document.getElementById('password');
-		const username = username_input?.value;
+		const error_message: any = document.getElementById('error');
+		const username = username_input?.value.trim().toLowerCase();
 		const password = password_input?.value;
-		const { data, error } = await supabase.auth.signUp({
-			email: username + '@fake-domain.com',
-			password: password
-		});
-		console.log(data, error);
+		if (userNameCheck(username)) {
+			error_message.innerText = userNameCheck(username);
+		} else {
+			error_message.innerText = '';
+			const { data, error } = await supabase.auth.signUp({
+				email: username + '@fake-domain.com',
+				password: password
+			});
+			console.log(data, error);
+		}
 
 	}
 	const signIn = async () => {
 		console.log('signing in');
 		const username_input: any = document.getElementById('username');
 		const password_input: any = document.getElementById('password');
-		const username = username_input?.value;
+		const error_message: any = document.getElementById('error');
+		const username = username_input?.value.trim().toLowerCase();
 		const password = password_input?.value;
-		const { data, error } = await supabase.auth.signInWithPassword({
-			email: username + '@fake-domain.com',
-			password: password
-		});
-		console.log(data, error);
-
+		if (userNameCheck(username)) {
+			error_message.innerText = userNameCheck(username);
+		} else {
+			error_message.innerText = '';
+			const { data, error } = await supabase.auth.signInWithPassword({
+				email: username + '@fake-domain.com',
+				password: password
+			});
+			console.log(data, error);
+		}
 }
 
 
@@ -45,6 +69,8 @@
 	<br /><br />
 	<button on:click={signUp}>Sign Up</button><br /><br />
 	<button on:click={signIn}>Login</button>
+	<br /><br />
+	<div class="error" id="error"></div>
 </main>
 <style>
 	main {
@@ -60,5 +86,9 @@
 		border-radius: 0.5em;
 		height: 2em;
 		width: 150px;
+	}
+	.error {
+		color: red;
+		font-size: larger;
 	}
 </style>
