3ce9e4fd301aa6ca355d028f2733d29aee5da872
Mark Burggraf <markb@mantisbible.com>
2022-11-04 07:15:40 -0700

    give running commentary of the signup/signin process

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,24 +19,62 @@
 		return "Username can only contain letters, numbers, and underscores";
 		return null;
 	}
+	const isValidEmail = (email: string) => {
+		return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+	}
 	const signUp = async () => {
 		console.log('signing up');
 		const username_input: any = document.getElementById('username');
 		const password_input: any = document.getElementById('password');
 		const error_message: any = document.getElementById('error');
+		const notes: any = document.getElementById('notes');
+		notes.innerHTML = '';
 		const username = username_input?.value.trim().toLowerCase();
 		const password = password_input?.value;
-		if (userNameCheck(username)) {
-			error_message.innerText = userNameCheck(username);
-		} else {
-			error_message.innerText = '';
-			const { data, error } = await supabase.auth.signUp({
-				email: username + '@fake-domain.com',
-				password: password
-			});
-			console.log(data, error);
-			if (data?.user) {
-				currentUser = data.user;
+		if (username.indexOf('@') === -1) { // username sign up
+			notes.innerHTML = 'User is signing up with username.<br/>';
+			if (userNameCheck(username)) {
+				error_message.innerText = userNameCheck(username);
+				notes.innerHTML += 'Username check failed.<br/>';
+			} else {
+				error_message.innerText = '';
+				const { data, error } = await supabase.auth.signUp({
+					email: username + '@fake-domain.com',
+					password: password
+				});
+				console.log(data, error);
+				if (data?.user) {
+					notes.innerHTML += 'User signed up.<br/>';
+					currentUser = data.user;
+				} else {
+					notes.innerHTML += 'User sign up failed.<br/>';
+					error_message.innerText = error?.message;
+				}
+			}
+		} else { // email sign up
+			notes.innerHTML = 'User is signing up with email.<br/>';
+			if (!isValidEmail(username)) {
+				error_message.innerText = 'Invalid email address';
+				notes.innerHTML += 'Email check failed.<br/>';
+			} else {
+				error_message.innerText = '';
+				const { data, error } = await supabase.auth.signUp({
+					email: username,
+					password: password
+				});
+				console.log(data, error);
+				if (data?.user) {
+					notes.innerHTML += 'User signed up.<br/>';
+					currentUser = data.user;
+					if (!currentUser.email_confirmed_at){
+						error_message.innerText = 'Please check your email to confirm your account.';
+						notes.innerHTML += 'User email not confirmed.<br/>';
+					}
+				} else {
+					notes.innerHTML += 'User sign up failed.<br/>';
+					notes.innerHTML += error?.message + '<br/>';
+					error_message.innerText = error?.message;
+				}
 			}
 		}
 	}
@@ -45,19 +83,51 @@
 		const username_input: any = document.getElementById('username');
 		const password_input: any = document.getElementById('password');
 		const error_message: any = document.getElementById('error');
+		const notes: any = document.getElementById('notes');
+		notes.innerHTML = '';
 		const username = username_input?.value.trim().toLowerCase();
 		const password = password_input?.value;
-		if (userNameCheck(username)) {
-			error_message.innerText = userNameCheck(username);
-		} else {
-			error_message.innerText = '';
-			const { data, error } = await supabase.auth.signInWithPassword({
-				email: username + '@fake-domain.com',
-				password: password
-			});
-			console.log(data, error);
-			if (data?.user) {
-				currentUser = data.user;
+		if (username.indexOf('@') === -1) { // username sign in
+			notes.innerHTML = 'User is signing in with username.<br/>';
+			if (userNameCheck(username)) {
+				error_message.innerText = userNameCheck(username);
+				notes.innerHTML += 'Username check failed.<br/>';
+			} else {
+				error_message.innerText = '';
+				const { data, error } = await supabase.auth.signInWithPassword({
+					email: username + '@fake-domain.com',
+					password: password
+				});
+				console.log(data, error);
+				if (data?.user) {
+					notes.innerHTML += 'User signed in.<br/>';
+					currentUser = data.user;
+				} else {
+					notes.innerHTML += 'User sign in failed.<br/>';
+					notes.innerHTML += error?.message + '<br/>';
+					error_message.innerText = error?.message;
+				}
+			}
+		} else { // email sign in
+			notes.innerHTML = 'User is signing in with email.<br/>';
+			if (!isValidEmail(username)) {
+				error_message.innerText = 'Invalid email address';
+				notes.innerHTML += 'Email check failed.<br/>';
+			} else {
+				error_message.innerText = '';
+				const { data, error } = await supabase.auth.signInWithPassword({
+					email: username,
+					password: password
+				});
+				console.log(data, error);
+				if (data?.user) {
+					notes.innerHTML += 'User signed in.<br/>';
+					currentUser = data.user;
+				} else {
+					notes.innerHTML += 'User sign in failed.<br/>';
+					notes.innerHTML += error?.message + '<br/>';
+					error_message.innerText = error?.message;
+				}
 			}
 		}
 }
@@ -89,13 +159,24 @@ onMount(() => {
 
 <main>
 	<h1>Demo: Supabase Login with Username</h1>
-	Username<br /><input id="username" type="text" placeholder="username" /><br />
-	<br />
-	Password<br /><input id="password" type="password" placeholder="password" /><br />
-	<br /><br />
-	<button on:click={signUp}>Sign Up</button><br /><br />
-	<button on:click={signIn}>Sign In</button><br /><br />
-	<button on:click={signOut}>Sign Out</button>
+
+	<!-- create table with no lines and no padding -->
+	<table style="border: 0; padding: 0;">
+		<tr>
+			<td style="border: 0;">
+				Username or Email<br /><input id="username" type="text" placeholder="username" /><br />
+				<br />
+				Password<br /><input id="password" type="password" placeholder="password" /><br />
+				<br /><br />
+				<button on:click={signUp}>Sign Up</button><br /><br />
+				<button on:click={signIn}>Sign In</button><br /><br />
+				<button on:click={signOut}>Sign Out</button>			
+			</td>
+			<td style="border: 0; padding: 2em; vertical-align: top;">
+				<div id="notes" class="notes"></div>
+			</td>
+		</tr>
+	</table>
 
 	<br /><br />
 	<div class="error" id="error"></div>
@@ -120,6 +201,10 @@ onMount(() => {
 	}
 	.error {
 		color: red;
+		font-size: larger;
+	}
+	.notes {
+		color: blue;
 		font-size: larger;
 	}
 </style>
